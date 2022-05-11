40748e820252c2e37fd561da05bee4be1e763dbd
Yoyo Yang <yoyo.truly@gmail.com>
2022-05-10 20:32:30 -0400

    Fix syntax

--- server/index.js
+++ server/index.js
@@ -1,30 +1,19 @@
 "use strict";
 
 const express = require("express");
+const bodyParser = require("body-parser");
 const PORT = 8080;
 
-const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 const db = require("./lib/in-memory-db");
-
-// The `data-helpers` module provides an interface to the database of tweets.
-// This simple interface layer has a big benefit: we could switch out the
-// actual database it uses and see little to no changes elsewhere in the code
-// (hint hint).
-//
-// Because it exports a function that expects the `db` as a parameter, we can
-// require it and pass the `db` parameter immediately:
 const DataHelpers = require("./lib/data-helpers.js")(db);
 
-// Update the dates for the initial tweets (data-files/initial-tweets.json).
 require("./lib/date-adjust")();
 
-// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-// so it can define routes that use it to interact with the data layer.
 const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
 app.use("/tweets", tweetsRoutes);
--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,9 +1,7 @@
 "use strict";
 
-// Simulates the kind of delay we see with network or filesystem operations
 const simulateDelay = require("./util/simulate-delay");
 
-// Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
 
