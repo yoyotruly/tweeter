141f062d6dd7597a13b1c1f98a94bb559d28f87d
Yoyo Yang <yoyo.truly@gmail.com>
2022-05-17 02:29:59 -0400

    Change counter color to red when it's negative

--- public/scripts/new-tweet-char-count.js
+++ public/scripts/new-tweet-char-count.js
@@ -2,8 +2,15 @@ $(document).ready(() => {
   $("#new-tweet__text").on("keyup", function() {
     const MAX_LEN = 140;
     const currentLen = $(this).val().length;
+    const remainingLen = MAX_LEN - currentLen;
 
     const charCountElem = $(this).parent().find(".counter");
-    charCountElem.val(MAX_LEN - currentLen);
+    charCountElem.val(remainingLen);
+
+    if (remainingLen < 0) {
+      charCountElem.css("color", "red");
+    } else {
+      charCountElem.css("color", "inherit");
+    }
   });
 });
