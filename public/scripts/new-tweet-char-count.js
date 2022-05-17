7c5f4d9fad7e83e0db691d54682cf2805bc632e2
Yoyo Yang <yoyo.truly@gmail.com>
2022-05-17 02:22:33 -0400

    Modify child traversal to use find

--- public/scripts/new-tweet-char-count.js
+++ public/scripts/new-tweet-char-count.js
@@ -3,7 +3,7 @@ $(document).ready(() => {
     const MAX_LEN = 140;
     const currentLen = $(this).val().length;
 
-    const charCountElem = $(this).parent().children("div").children(".counter");
+    const charCountElem = $(this).parent().find(".counter");
     charCountElem.val(MAX_LEN - currentLen);
   });
 });
