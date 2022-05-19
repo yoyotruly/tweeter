17bca205de4e40b216876c76915a68dea0786102
Yoyo Yang <yoyo.truly@gmail.com>
2022-05-18 22:24:21 -0400

    Fix tweet icons display

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -11,10 +11,10 @@ const createTweetElement = (tweet) => {
       <p class="tweet__text">${tweet.content.text}</p>
       <footer class="tweet__footer">
         <time class="tweet__time">${tweet.created_at}</time>
-        <div class="tweet__icons">
-          <i class="fa-solid fa-flag"></i>
-          <i class="fa-solid fa-retweet"></i>
-          <i class="fa-solid fa-heart"></i>
+        <div>
+          <i class="fa-solid fa-flag tweet__icon"></i>
+          <i class="fa-solid fa-retweet tweet__icon"></i>
+          <i class="fa-solid fa-heart tweet__icon"></i>
         </div>
       </footer>
     </article>
@@ -28,37 +28,16 @@ const renderTweets = (tweets) => {
   });
 };
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
-renderTweets(data);
-
-
 $("#new-tweet__form").submit(function(event) {
   event.preventDefault();
 
   const text = $(this).serialize();
   $.post("/tweets", text);
 });
+
+const loadTweets = () => {
+  return $.get("/tweets")
+    .then((tweets) => renderTweets(tweets));
+};
+
+loadTweets();
