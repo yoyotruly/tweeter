7be58a8be6445e3a05f1bf99e75843e749dacb35
Yoyo Yang <yoyo.truly@gmail.com>
2022-05-18 21:18:17 -0400

    Add feature to render tweets via jquery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,56 +1,63 @@
-// /*
-//  * Client-side JS logic goes here
-//  * jQuery is already loaded
-//  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
-//  */
-
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
+const createTweetElement = (tweet) => {
+  return `
+    <article class="tweet">
+      <header>
+        <div class="tweet__user">
+          <img src=${tweet.user.avatars} alt="avatar">
+          <p>${tweet.user.name}</p>
+        </div>
+        <div class="tweet__handle">${tweet.user.handle}</div>
+      </header>
+      <p class="tweet__text">${tweet.content.text}</p>
+      <footer>
+        <time>${tweet.created_at}</time>
+        <div>
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+  `;
 };
 
-$(document).ready(function() {
-  
-  const createTweetElement = (tweet) => {
+const renderTweets = (tweets) => {
+  tweets.map((tweet) => {
+    const $tweetElement = createTweetElement(tweet);
+    $('#tweets-container').append($tweetElement);
+  });
+};
 
-    return `
-      <article class="tweet">
-        <header>
-          <div class="tweet__user">
-            <img src=${tweet.user.avatars} alt="avatar">
-            <p>${tweet.user.name}</p>
-          </div>
-          <div class="tweet__handle">${tweet.user.handle}</div>
-        </header>
-        <p class="tweet__text">${tweet.content.text}</p>
-        <footer>
-          <time>${tweet.created_at}</time>
-          <div>
-            <i class="fa-solid fa-flag"></i>
-            <i class="fa-solid fa-retweet"></i>
-            <i class="fa-solid fa-heart"></i>
-          </div>
-        </footer>
-      </article>
-    `;
-  };
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
 
-  
-  const $tweet = createTweetElement(tweetData);
-  console.log($tweet);
-  $('#tweets-container').append($tweet);
+// $(document).ready(function() {
+//   renderTweets(data);
+// });
 
-});
-
-
-
+renderTweets(data);
 
 
 // $("#new-tweet").submit(function(event) {
