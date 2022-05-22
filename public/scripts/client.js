bf3f3de7e4c0b3aadbfa79ddaa9dbdf79f6cd112
Yoyo Yang <yoyo.truly@gmail.com>
2022-05-21 21:28:47 -0400

    Add error message handling and comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,3 +1,7 @@
+/** Convert input to plain text to prevent cross-site scripting.
+ * @param {string} str Any text string
+ * @returns {string} Plain text (HTML tags won't be interpreted by the browswer)
+*/
 const convertText = (str) => {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
@@ -5,6 +9,11 @@ const convertText = (str) => {
   return div.innerHTML;
 };
 
+/**
+ * Create a complete tweet HTML element.
+ * @param {Object} tweet Tweet object containing user info, time and tweet text
+ * @returns HTML element of a tweet
+ */
 const createTweetElement = (tweet) => {
   const avatar = convertText(tweet.user.avatars);
   const name = convertText(tweet.user.name);
@@ -34,31 +43,70 @@ const createTweetElement = (tweet) => {
   `;
 };
 
+/**
+ * Render individual tweet on the very top of the tweets-container.
+ * @param {Object} tweet Tweet object containing user info, time and tweet text
+ * @returns None
+ */
 const renderTweet = (tweet) => {
   const $tweetElement = createTweetElement(tweet);
   $("#tweets-container").prepend($tweetElement);
 };
 
-const loadTweets = async (path) => {
-  const tweets = await $.ajax(path, { method: "GET" });
+/**
+ * Fetch and load all tweets from a given path.
+ * @param {*} route The route to get all tweets
+ * @returns New DOM with all tweets prepended to the tweets-container
+ */
+const loadTweets = async (route) => {
+  const tweets = await $.ajax(route, { method: "GET" });
 
   return tweets.map((tweet) => renderTweet(tweet));
 };
 
+/**
+ * Valid if a tweet is empty or exceeds character limit.
+ * @param {*} tweetText Tweet text user submitted
+ * @returns {string} Appropriate error message
+ */
+const validateTweet = (tweetText) => {
+  if (!tweetText && tweetText !== 0) {
+    return "Oops, tweet can't be empty.";
+  } else if (tweetText.length > 140) {
+    return "Got too much to say? Try break them down into separate tweets.";
+  }
+};
 
+/* ------ Main Functions ------ */
 loadTweets("/tweets");
 
 $("#new-tweet__form").submit(async function(event) {
   event.preventDefault();
+  
+  /* on invalid submission, prompt error message and stop progressing further */
+  const text = $("#new-tweet__text").val();
+  const errorMsg = validateTweet(text);
+  if (errorMsg) {
+    return $("#new-tweet__error")
+      .text(errorMsg)
+      .prepend("<i class='fa-solid fa-circle-exclamation'></i> ")
+      .animate({ "margin-left": "1.5em" }, 100)  // mimic shake effect
+      .animate({ "margin-left": "1em" }, 100)
+      .animate({ "margin-left": "1.5em" }, 100)
+      .animate({ "margin-left": "1em" }, 100);
+  }
+  
+  /* on successful re-submission, remove any previous error message */
+  const errorContent = $("#new-tweet__error").contents();
+  if (errorContent) {
+    errorContent.remove();
+  }
 
-  const text = $(this).serialize();
-  const newTweet = await $.post("/tweets", text);
+  /* incrementally submit and render new tweet */
+  const serializedText = $(this).serialize();
+  const newTweet = await $.post("/tweets", serializedText);
   renderTweet(newTweet);
 
-  $("#new-tweet__form").trigger("reset");
+  /* clear input text area after successful submission */
+  $(this).trigger("reset");
 });
-
-
-// const validateTweet = (tweetText) => {
-//   if (tweetText)
-// }
